d3fa15bf4de08304ac3b61d795dd639f99b63863
okeseyi8 <okeseyi8@gmail.com>
2024-12-21 11:06:34 +0100

    cleaned up other conflicts

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,16 +1,9 @@
 import React, { useState } from "react";
 import "./Auth.css";
-<<<<<<< HEAD
 // import loginMain from "../../assets/login-main.png";
 // import PasswordInput from "../../components/PasswordInput/PasswordInput";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-=======
-import loginMain from "../../assets/login-main.png";
-import PasswordInput from "../../components/PasswordInput/PasswordInput";
-import { Link } from "react-router-dom";
-
->>>>>>> 534ca26af595f7841ec3c3067fa58f1ca9740446
 import { IoPersonOutline } from "react-icons/io5";
 import { PiStudentLight } from "react-icons/pi";
 import { LiaChalkboardTeacherSolid } from "react-icons/lia";
@@ -18,10 +11,7 @@ import { LiaChalkboardTeacherSolid } from "react-icons/lia";
 const Login = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-<<<<<<< HEAD
     const navigate = useNavigate()
-=======
->>>>>>> 534ca26af595f7841ec3c3067fa58f1ca9740446
 
 	const [role, setRole] = useState("admin");
 
@@ -54,11 +44,7 @@ const Login = () => {
 					</div>
 				</section>
 
-<<<<<<< HEAD
 				<form onSubmit={(e)=> e.preventDefault()}>
-=======
-				<form>
->>>>>>> 534ca26af595f7841ec3c3067fa58f1ca9740446
 					<label htmlFor="email">
 						<span>Email Address</span>
 						<input
@@ -72,31 +58,19 @@ const Login = () => {
 
 					<label htmlFor="password">
 						<span>Password</span>
-<<<<<<< HEAD
 					    {/* <PasswordInput
-=======
-						<PasswordInput
->>>>>>> 534ca26af595f7841ec3c3067fa58f1ca9740446
 							placeholder="****************"
 							id="password"
 							value={password}
 							onChange={(e) => setPassword(e.target.value)}
-<<<<<<< HEAD
 						/> */}
-=======
-						/>
->>>>>>> 534ca26af595f7841ec3c3067fa58f1ca9740446
 					</label>
 
 					<Link to="/forgotpassword" className="forgot-password">
 						Forgot Password?
 					</Link>
 
-<<<<<<< HEAD
 					<button onClick={() => navigate("/dashboard")}> Login</button>
-=======
-					<button>Login</button>
->>>>>>> 534ca26af595f7841ec3c3067fa58f1ca9740446
 
 					<h6>
 						Don't have an account? <Link to="/signup">Sign Up</Link>
@@ -105,21 +79,12 @@ const Login = () => {
 			</main>
 			<aside>
 				<h1>LUNAR SMS - Where Learning Meets Management.</h1>
-<<<<<<< HEAD
                 <button onClick={() => navigate("/dashboard")}> Login</button>
 
 				{/* <img src={loginMain} alt="" /> */}
-=======
-
-				<img src={loginMain} alt="" />
->>>>>>> 534ca26af595f7841ec3c3067fa58f1ca9740446
 			</aside>
 		</div>
 	);
 };
 
-<<<<<<< HEAD
 export default Login;
-=======
-export default Login;
->>>>>>> 534ca26af595f7841ec3c3067fa58f1ca9740446
