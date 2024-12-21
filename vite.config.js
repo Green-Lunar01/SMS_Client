141dc987a473c9b5fca70d52f3e87a0d350a002e
okeseyi8 <okeseyi8@gmail.com>
2024-12-21 10:48:25 +0100

    resolve merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import svgr from 'vite-plugin-svgr' 
 
 // https://vitejs.dev/config/
@@ -14,10 +13,3 @@ export default defineConfig({
     }),
   ], 
 })
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 534ca26af595f7841ec3c3067fa58f1ca9740446
