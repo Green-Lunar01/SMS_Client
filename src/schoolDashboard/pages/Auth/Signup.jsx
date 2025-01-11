401ef3ecb6c249df0f30b4d6f680e79c3ca4cadd
Bamid3v <ayobamiorioke@gmail.com>
2025-01-11 17:35:47 +0100

    login and signup endpoint done

--- src/schoolDashboard/pages/Auth/Signup.jsx
+++ src/schoolDashboard/pages/Auth/Signup.jsx
@@ -1,14 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./Auth.css";
 import authMain from "../../assets/auth-main.png";
 import PasswordInput from "../../components/PasswordInput/PasswordInput";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { UserContext } from "../../context/userContext";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import Spinner from "../../components/Spinner/Spinner";
 
 const Signup = () => {
 	const [email, setEmail] = useState("");
 	const [schoolName, setSchoolName] = useState("");
-	const [role, setRole] = useState("");
+	const [role, setRole] = useState("School Owner");
 	const [password, setPassword] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [terms, setTerms] = useState(false);
+
+	const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+	const { setUser, setUserToken } = useContext(UserContext);
+	const navigate = useNavigate();
+
+	const signup = async () => {
+		if (!terms) {
+			toast.error("Please accept terms and conditions");
+			return;
+		}
+
+		if (!email || !password || !role || !schoolName) {
+			toast.error("All fields are required");
+			return;
+		}
+
+		setLoading(true);
+
+		try {
+			const response = await axios.post(`${BASE_API_URL}/school/signup`, {
+				email,
+				school_name: schoolName,
+				pswd: password,
+				role,
+			});
+			toast.success("Sign up successful");
+			// console.log(response);
+			setLoading(false);
+			navigate("/school/login");
+		} catch (err) {
+			toast.error(err.message);
+			console.log(err);
+			setLoading(false);
+		}
+	};
 
 	return (
 		<div className="auth signup">
@@ -46,10 +88,11 @@ const Signup = () => {
 						<select
 							name="role"
 							id="role"
+							value={role}
 							onChange={(e) => setRole(e.target.value)}
 						>
-							<option value="owner">School Owner</option>
-							<option value="principal">Principal</option>
+							<option value="School Owner">School Owner</option>
+							<option value="Principal">Principal</option>
 						</select>
 					</label>
 
@@ -64,17 +107,25 @@ const Signup = () => {
 					</label>
 
 					<aside>
-						<input type="checkbox" name="terms" id="terms" />
+						<input
+							type="checkbox"
+							name="terms"
+							id="terms"
+							onChange={(e) => setTerms(e.target.checked)}
+						/>
 						<p>
 							I accept the <span>Terms</span> and{" "}
 							<span>Conditions</span>
 						</p>
 					</aside>
 
-					<button>Sign Up</button>
+					<button onClick={signup} disabled={loading} type="button">
+						{loading ? <Spinner /> : "Sign Up"}
+					</button>
 
 					<h6>
-						Already have an account? <Link to="/login">Login</Link>
+						Already have an account?{" "}
+						<Link to="/school/login">Login</Link>
 					</h6>
 				</form>
 			</main>
--- src/schoolDashboard/pages/Dashboard/Class/Class.jsx
+++ src/schoolDashboard/pages/Dashboard/Class/Class.jsx
@@ -1,9 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./Class.css";
 import AllClasses from "./AllClasses/AllClasses";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../../../components/Spinner/Spinner";
+import { UserContext } from "../../../context/userContext";
 
 const Class = () => {
 	const [tab, setTab] = useState("create");
+	const [loading, setLoading] = useState(false);
+	const [className, setClassName] = useState();
+	const [schoolFees, setSchoolFees] = useState();
+	const [classTeacher, setClassTeacher] = useState();
+
+	const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+	const { userToken } = useContext(UserContext);
+	const navigate = useNavigate();
+
+	const createClass = async () => {
+		if (!className) {
+			toast.error("All fields are required");
+			return;
+		}
+		setLoading(true);
+		console.log({ userToken });
+
+		try {
+			const response = await axios.post(
+				`${BASE_API_URL}/school/classes/create`,
+				{
+					class_name: className,
+					teacher_id: 0,
+				},
+				{
+					headers: {
+						Authorization: `${userToken}`,
+					},
+				},
+			);
+			toast.success("class created successfully");
+			console.log(response);
+			setLoading(false);
+		} catch (err) {
+			toast.error(err.message);
+			console.log(err);
+			setLoading(false);
+		}
+	};
 
 	return (
 		<div className="class-screen">
@@ -33,6 +77,8 @@ const Class = () => {
 								type="text"
 								id="class-name"
 								placeholder="e.g J.S.S 1"
+								value={className}
+								onChange={(e) => setClassName(e.target.value)}
 							/>
 						</label>
 						<label htmlFor="term-fee">
@@ -40,18 +86,29 @@ const Class = () => {
 							<input
 								type="text"
 								id="term-fee"
+								value={schoolFees}
+								onChange={(e) => setSchoolFees(e.target.value)}
 								placeholder="e.g N40,000"
 							/>
 						</label>
 						<label htmlFor="class-teacher">
 							<p>Class Teacher *</p>
-							<select name="class-teacher" id="class-teacher">
+							<select
+								name="class-teacher"
+								id="class-teacher"
+								value={classTeacher}
+								onChange={(e) =>
+									setClassTeacher(e.target.value)
+								}
+							>
 								<option value="Mr John">Mr John</option>
 								<option value="Mrs Jane">Mrs Jane</option>
 							</select>
 						</label>
 
-						<button>Create</button>
+						<button onClick={createClass} disabled={loading}>
+							{loading ? <Spinner /> : "Create Class"}
+						</button>
 					</div>
 				</>
 			)}
